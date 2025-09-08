4ff7f46d1f37c6494c44cbfcec1570fab9299b18
Jagdish <jagdishjena93@gmail.com>
2025-09-08 11:22:33 +0530

    Add Success and Cancel pages

--- src/components/Cancel.jsx
+++ src/components/Cancel.jsx
@@ -1,7 +1,13 @@
+// components/Cancel.jsx
 import React from "react";
 
 const Cancel = () => {
-  return <div>Cancel</div>;
+  return (
+    <div className="text-center mt-5">
+      <h1 className="text-danger">❌ Payment Cancelled</h1>
+      <p>You can try again anytime.</p>
+    </div>
+  );
 };
 
 export default Cancel;
